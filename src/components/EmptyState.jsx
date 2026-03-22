db4057adc3d9e32c5d644d24b04fa9d02c2b5186
027123-prog <nilswienstroer@hotmail.de>
2026-03-22 19:36:22 +0100

    Delete src directory

--- src/components/EmptyState.jsx
+++
@@ -1,8 +0,0 @@
-export default function EmptyState({ title, description }) {
-  return (
-    <div className="panel p-8 text-center">
-      <h3 className="font-display text-2xl font-semibold text-ink">{title}</h3>
-      <p className="mx-auto mt-3 max-w-xl text-sm text-ink/70">{description}</p>
-    </div>
-  );
-}
